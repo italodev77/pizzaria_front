c9c2761634dc5a41b14f50c32d4ec8b693e3957d
devitalo <italo_ariel_dev77@outlook.com>
2024-12-04 21:17:02 -0300

    feat: criação da tela de cadastro de produtos e implementação do tost

--- src/app/dashboard/product/component/form/index.tsx
+++ src/app/dashboard/product/component/form/index.tsx
@@ -1,7 +1,9 @@
 "use client";
 import { Button } from "@/app/dashboard/components/button";
 import style from "./style.module.scss";
-
+import { api } from "@/services/api";
+import { toast } from "sonner";
+import { error } from "console";
 interface ICategoryProps {
   id: string;
   description: string;
@@ -11,14 +13,39 @@ interface Props {
   categories: ICategoryProps[];
 }
 export function Form({ categories }: Props) {
+  async function handleRegisterProduct(formData: FormData) {
+    const categories = formData.get("categories");
+    const description = formData.get("description");
+    const price = formData.get("price");
+    const amount = formData.get("amount");
+
+    if (!description || !categories || !price || !amount) {
+      toast.warning("Preencha todos os campos!");
+      return;
+    }
+
+    const data = {
+      description: description,
+      price: price,
+      amount: amount,
+      categoryId: categories,
+    };
+
+    await api.post("/products", data).catch(() => {
+      toast.warning("Falha ao cadastrar esse produto");
+      return;
+    });
+
+    toast.success("Produto cadastrado com sucesso");
+  }
   return (
     <main className={style.container}>
       <h1>Novo Produto:</h1>
 
-      <form className={style.form}>
+      <form className={style.form} action={handleRegisterProduct}>
         <select name="categories">
-          {categories.map((category, index) => (
-            <option key={category.id} value={index}>
+          {categories.map((category) => (
+            <option key={category.id} value={category.id}>
               {category.description}
             </option>
           ))}
